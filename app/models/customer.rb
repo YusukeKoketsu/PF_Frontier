74e9fd90a96b40a7a65d5cc4de9c17b6cda9fffe
YusukeKoketsu <koketsuyusuki4084@gmail.com>
2022-06-06 03:02:35 +0000

    add customers image

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,17 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+
+#会員アイコン画像をアップロード
+def get_image(size)
+     unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no-image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+     end
+     image.variant(resize:size).processed
+end
+
+
 end
